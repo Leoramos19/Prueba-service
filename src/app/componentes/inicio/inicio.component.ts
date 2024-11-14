f62cfef6ae6cec0ee17a1c0e90ef3c46ffc92649
Leonardo Ramos <leonardo.ramos.valdivia@cftmail.cl>
2024-11-14 11:07:43 -0300

    Prueba de servicio angular

---
+++ src/app/componentes/inicio/inicio.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
+import { PruebaService } from '../../servicios/prueba/prueba.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+//Instanciar en una variable el formulario reactivo
+formularioForm;
+//FIN Instanciar en una variable el formulario reactivo
+//Crear un constructor: Esto permita inicializar variables con atributos y valores
+constructor(private formBuilder: FormBuilder, private pruebaSrv: PruebaService ){
+  this.formularioForm = this.formBuilder.group({
+    nombre: '',
+    apellido: ''
+  });
+}  
+//FIN Crear un constructor: Esto permita inicializar variables con atributos y valores
+
+//Creamos un programa principal al cargar la web. Este se ejecuta al momento de cargar la página Web
+ngOnInit() {
+  console.log("hola");
+}
+//Fin programa principal
+
+enviarDatos(){
+  //Podemos crear una variable donde almacenaremos los datos capturados (opcional)
+  let datos_formulario = this.formularioForm.value;
+  //Fin variable datos formulario
+
+  //Imprimir por consola los datos capturados en la variable
+  console.log(datos_formulario);
+  console.warn('Los datos capturados son:', this.formularioForm.value);
+  this.formularioForm.reset();
+  
+  //Fin imprimir por consola
+}
+
+  //Funcion que activa una función del servicio Prueba
+  activar_invocable1(){
+    let dato_desde_servicio;
+    dato_desde_servicio = this.pruebaSrv.Invocable_1();
+    console.log(dato_desde_servicio);
+  }
+  activar_invocable2(){
+    let dato_desde_servicio;
+    dato_desde_servicio = this.pruebaSrv.Invocable_2();
+    console.log(dato_desde_servicio);
+  }
+  activar_invocable3(){
+    let dato_desde_servicio;
+    dato_desde_servicio = this.pruebaSrv.Invocable_3();
+    console.log(dato_desde_servicio);
+  }
+}
---
+++ src/app/servicios/prueba/prueba.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PruebaService {
+
+  constructor(private http: HttpClient) { }
+  
+  Invocable_1(){
+    let dato = "Funcion 1 invocada";
+    return dato;
+  }
+  Invocable_2(){
+    let dato = "Funcion 2 invocada";
+    return dato;
+  }
+  Invocable_3(){
+    let dato = "Funcion 3 invocada";
+    return dato;
+  }
+}
